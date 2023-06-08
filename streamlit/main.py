f685bdeee3527e434eaf6dc8c16610e240471694
JuanGouveia <jdegouveiaf@gmail.com>
2023-06-08 03:49:52 +0200

    terminado visualizacion, casi terminado queries de sql y empezando streamlit

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,9 +1,37 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-import pylab as plt
+#import pylab as plt
 import webbrowser
 import base64
 import io
 
-st.title('Clase Streamlit')
+
+
+circuits = pd.read_csv('clean_data/circuits_clean.csv')
+
+
+
+st.sidebar.image(Image.open('images/formula_one.png'))
+
+
+def show_home():
+    st.title('Formula One Data Visualization')
+
+def show_calendar():
+    st.title('Calendar')
+
+def show_races():
+    st.title('Races')
+
+
+sel_page = st.sidebar.radio('Information', ('Home', 'Calendar', 'Races'))
+
+if sel_page == 'Home':
+    show_home()
+
+elif sel_page == 'Calendar':
+    show_calendar()
+
+elif sel_page == 'Races':
+    show_races()
